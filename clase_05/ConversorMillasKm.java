f699e2fd0c6d31f3ed3e8ad620517e0e246b56f0
diana <dianacarhuamanta03@gmail.com>
2023-06-07 17:34:53 -0500

    conversor millas en km

--- clase_05/ConversorMillasKm.java
+++ clase_05/ConversorMillasKm.java
@@ -9,9 +9,9 @@ public class ConversorMillasKm {
 
         System.out.println("Ingrese una distancia en millas");
         double dataLoadMiles = dataLoad.nextDouble();
+        double km = 1.60934;
 
         if (dataLoadMiles > 0){
-            double km = 1.60934;
             double kmConvert = dataLoadMiles * km;
             System.out.println("Los km de las millas ingresadas son : " + kmConvert);
         } else {
