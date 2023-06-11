fb825e6fd642e9a07cc5712ae2d71e0c6d144a5f
diana <diana.carhuamanta@deister.es>
2023-06-11 14:48:39 -0500

    show number

---
+++ clase_06/ShowNum.java
@@ -0,0 +1,10 @@
+package clase_06;
+public class ShowNum {
+    public static void main(String[] args) {
+        Integer num = 1;
+        while (num < 101) {
+            System.out.println(num);
+            num++;
+        }
+    }
+}
