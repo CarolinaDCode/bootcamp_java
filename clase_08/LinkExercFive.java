18e066e8b0593191155d23c97516f3e41808f1a6
diana <diana.carhuamanta@deister.es>
2023-06-18 23:19:34 -0500

    ejercicios 4 y 5 clase 8

---
+++ clase_08/LinkExercFive.java
@@ -0,0 +1,7 @@
+package clase_08;
+
+public class LinkExercFive {
+    public static void ExerciseFour(){
+        String link = "https://github.com/CarolinaDCode/springBoot-exercises/blob/main/src/main/java/academy/atl/bootcamp/RockPaperScissor.java";
+    }
+}
---
+++ clase_08/LinkExercFour.java
@@ -0,0 +1,7 @@
+package clase_08;
+
+public class LinkExercFour {
+    public static void ExerciseFour(){
+        String link= "https://github.com/CarolinaDCode/springBoot-exercises/blob/main/src/main/java/academy/atl/bootcamp/MainController.java";
+    }
+}
