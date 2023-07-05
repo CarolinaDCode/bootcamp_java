9c444f42aff66521b93dbec21119a2e6bf62aad7
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-07-05 01:24:24 -0500

    primer examen

--- examen_01/Exercise_01.java
+++ examen_01/Exercise_01.java
@@ -1,2 +1,24 @@
-package examen_01;public class Exercise_01 {
+package examen_01;
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+        //Encuentra la suma de los números impares menores a 1000
+        /**
+        int sumaImpares = 0;
+        for(int i=1; i < 1000; i+=2){
+            sumaImpares += i;
+        }
+        System.out.println(sumaImpares);
+        */
+
+        //Encuentra la suma de los múltiplos de 3 menores a 1000
+        int sumaDiv3 = 0;
+        for(int j = 0; j < 1000; j++){
+            if(j % 3 == 0){
+                sumaDiv3 += j;
+            }
+        }
+        System.out.println(sumaDiv3);
+    }
 }
--- examen_01/Usuario.java
+++ examen_01/Usuario.java
@@ -1,2 +1,67 @@
-package examen_01;public class Usuario {
+package examen_01;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private int telefono;
+
+    public Usuario(){};
+    public Usuario(int id, String nombre, String apellido, String email, String password, int telefono){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.telefono = telefono;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setEmail(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void impDatosUsuario(){
+        System.out.print("\nId: " + id +
+                "\nNombre: " + nombre +
+                "\nApellido: "  + apellido +
+                "\nEmail: "  + email   +
+                "\nTeléfono: " + telefono);
+    }
 }
