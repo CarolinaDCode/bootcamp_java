1b730e0fe9908f1e4a3b8c7eb56ad186f4422f5e
Diana Carhuamanta <dianacarhuamanta03@gmail.com>
2023-07-17 23:49:09 -0500

    creacion de tablas a partir del primer diagrama ER

--- clase_15/PrimerDiagramaER.java
+++ clase_15/PrimerDiagramaER.java
@@ -1,2 +1,8 @@
-package clase_15;public class PrimerDiagramaER {
+package clase_15;
+
+public class PrimerDiagramaER {
+    /**
+     * Terminar la creación de cada una de las tablas y relacionaciones de este diagrama de ER (Tienda Informatica)
+     */
+    String url = "";
 }
