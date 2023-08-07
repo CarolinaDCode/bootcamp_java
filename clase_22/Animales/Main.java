924b7af1198d01512f048c9cdc5942a7a3cc6b7e
Diana Carhuamanta <68792144+CarolinaDCode@users.noreply.github.com>
2023-08-06 21:57:05 -0500

    Update Main.java

--- clase_22/Animales/Main.java
+++ clase_22/Animales/Main.java
@@ -1,6 +1,11 @@
 package Animales;
 
 public class Main {
+
+    /**
+     * INTERFACES
+     * LINK DEL VIDEO: "https://www.twitch.tv/videos/1883244220"
+     */
     public static void main(String[] args) {
         Gorrion gorrion = new Gorrion();
         gorrion.aletear();
