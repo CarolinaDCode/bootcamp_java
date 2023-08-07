a79e8a1db1aba333fc0d38b7847e96864187cd5f
diana <diana.carhuamanta@deister.es>
2023-08-06 22:44:22 -0500

    link del video

--- clase_23/Main.java
+++ clase_23/Main.java
@@ -2,6 +2,11 @@ package clase_23;
 
 public class Main {
 
+    /**
+     * SINGLETON
+     * Link del video = "https://www.twitch.tv/videos/1886682199"
+     * @param args
+     */
     public static void main(String[] args) {
 
         float euros = CurrencyConverter.getInstance().convertirUsdToEur(10);
