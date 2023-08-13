a9db8d3d13ffc4d87869e849500036a54187927a
diana <diana.carhuamanta@deister.es>
2023-08-13 16:09:15 -0500

    excersise 2

--- clase_25/ejercicio_02/ContactBook.java
+++ clase_25/ejercicio_02/ContactBook.java
@@ -1,2 +1,42 @@
-package clase_25.ejercicio_02;public class ContactBook {
+package clase_25.ejercicio_02;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ContactBook {
+    public static void main(String[] args){
+        /**
+         * 2. Agenda de contactos:
+         * Desarrolla una aplicación de agenda de contactos
+         * en la que los usuarios puedan agregar nombres y números
+         * de teléfono. Implementa funciones para agregar,
+         * buscar y eliminar contactos utilizando un HashMap
+         * donde los nombres son las clave y los números son los valores
+         */
+
+        Map<String, Persona> listaDeContactos = new HashMap<>();
+
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 0; i < 3; i++){
+            System.out.println("Ingrese un DNI: ");
+            String dni = teclado.nextLine();
+            System.out.println("Ingrese un teléfono: ");
+            String telef = teclado.nextLine();
+            System.out.println("Ingrese un nombre: ");
+            String name = teclado.nextLine();
+
+
+            Persona persona = new Persona();
+            persona.setNombre(name);
+            persona.setDni(dni);
+            persona.setTelefono(telef);
+            listaDeContactos.put(dni, persona);
+        }
+
+        System.out.println("Ingrese el DNI a buscar");
+        String dni = teclado.nextLine();
+        Persona persona = listaDeContactos.get(dni);
+        System.out.println(persona.resumen());
+    }
 }
--- clase_25/ejercicio_02/Persona.java
+++ clase_25/ejercicio_02/Persona.java
@@ -1,2 +1,38 @@
-package clase_25.ejercicio_02;public class Persona {
+package clase_25.ejercicio_02;
+
+public class Persona {
+    private String nombre;
+    private String dni;
+
+    private String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String resumen(){
+        return "Nombre: " + nombre + "\n" +
+                "DNI " + dni + "\n" +
+                "Teléfono: " + telefono + "\n";
+    }
 }
