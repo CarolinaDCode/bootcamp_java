a2164b7eea162d0889abc7e5f1588210cb5f0627
diana <diana.carhuamanta@deister.es>
2023-08-13 16:09:56 -0500

    excersise 03

--- clase_25/ejercicio_03/Main.java
+++ clase_25/ejercicio_03/Main.java
@@ -1,2 +1,27 @@
-package clase_25.ejercicio_03;public class Main {
+package clase_25.ejercicio_03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args){
+        List<String> listaDeNombres = new ArrayList<>();
+        listaDeNombres.add("Lucas");
+        listaDeNombres.add("Sabrina");
+        listaDeNombres.add("Pepe");
+
+        listaDeNombres.stream()
+                      .map(nombre -> nombre.toUpperCase())
+                      .collect(Collectors.toList());
+        /** listaDeNombres.stream().map(nombre -> nombre.toUpperCase()).toList(); */
+
+        /**
+        List<String> nombresEnMayuscula = new ArrayList<>();
+        listaDeNombres.forEach(nombre ->
+                nombresEnMayuscula.add(nombre.toUpperCase()));
+        nombresEnMayuscula.forEach(nombre -> System.out.println(nombre));
+         */
+    }
 }
