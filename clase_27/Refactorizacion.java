f525148bf48319637b59e13b1cf5ce6c740e7863
diana <diana.carhuamanta@deister.es>
2023-08-21 00:06:57 -0500

    refactor

---
+++ clase_27/Refactorizacion.java
@@ -0,0 +1,11 @@
+package clase_27;
+
+public class Refactorizacion {
+
+    public static void main(String[] args) {
+        /**
+         * En esta clase se realizó la refactorización del juego El ahorcado
+         * */
+        String link = "https://github.com/CarolinaDCode/testing_professional";
+    }
+}
