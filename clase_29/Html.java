8685f8a1784a578137595db026c1820ac3c17a67
diana <diana.carhuamanta@deister.es>
2023-08-27 20:28:32 -0500

    html

--- clase_29/Html.java
+++ clase_29/Html.java
@@ -1,2 +1,10 @@
-package clase_29;public class html {
+package clase_29;
+
+public class Html {
+    public static void main(String[] args) {
+        /**
+         * HTML
+         * listas, formularios, iframes mapas, iframes video, tablas.
+         * */
+    }
 }
