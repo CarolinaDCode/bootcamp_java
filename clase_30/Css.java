20687105c80de1cee5e04dce9a578caaa28a92ef
diana <diana.carhuamanta@deister.es>
2023-08-27 20:53:47 -0500

    Css

--- clase_30/Css.java
+++ clase_30/Css.java
@@ -1,2 +1,10 @@
-package clase_30;public class css {
+package clase_30;
+
+public class Css {
+    public static void main(String[] args) {
+        /**
+         * CSS
+         * id, class, nivel de espcificidad
+         * */
+    }
 }
