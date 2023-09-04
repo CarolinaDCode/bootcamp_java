68f9854de93f52ea3d99f5ae8b3e3a700a393925
diana <diana.carhuamanta@deister.es>
2023-09-03 21:10:19 -0500

    garbage collector

--- clase_34/GarabeCollector.java
+++ clase_34/GarabeCollector.java
@@ -1,2 +1,11 @@
-package clase_34;public class GarabeCollector {
+package clase_34;
+
+public class GarabeCollector {
+    /**
+     * Garbage Collector
+     * En esta clase aprendimos lo que es el Garbage collector,
+     * como trabaja internamente con aquellos espacios en memoria ocupados
+     * pero no utilizados, no enlazados a ninguna variable.
+     * Tambien realizamos un pequeño Programa de Escritorio con Java.
+     * */
 }
