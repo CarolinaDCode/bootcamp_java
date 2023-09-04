6704f13beb1041ef87b8328736aa1908d4f18ca7
diana <diana.carhuamanta@deister.es>
2023-09-03 21:09:30 -0500

    Principios Solid

--- clase_33/PrincipiosSolid.java
+++ clase_33/PrincipiosSolid.java
@@ -1,2 +1,12 @@
-package clase_33;public class PrincipiosSolid {
+package clase_33;
+
+public class PrincipiosSolid {
+    /**
+     * Principios Solid
+     * - PRINCIPIO DE RESPONSABILIDAD UNICA
+     * - PRINCIPIO DE ABIERTO - CERRADO
+     * - PRINCIPIO DE SUSTITUCION DE LISKOV
+     * - PRINCIPIO DE SEGREGACIÓN DE iNTERFAZ
+     * - PRINCIPIO DE INVERSION DE DEPENDENCIA
+     * */
 }
